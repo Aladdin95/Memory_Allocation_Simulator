5ae3ad6fd39a472e00ad492580d6bd3fc76b1f05
Abdullah-A-Elsayed <abdullah.ali26@hotmail.com>
2018-04-20 00:24:11 +0200

    holes table is made

--- memory allocation/Form1.Designer.cs
+++ memory allocation/Form1.Designer.cs
@@ -29,18 +29,45 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.n_holes = new System.Windows.Forms.TextBox();
+            this.panel = new System.Windows.Forms.TableLayoutPanel();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // n_holes
+            // 
+            resources.ApplyResources(this.n_holes, "n_holes");
+            this.n_holes.Name = "n_holes";
+            this.n_holes.TextChanged += new System.EventHandler(this.n_holes_TextChanged);
+            // 
+            // panel
+            // 
+            resources.ApplyResources(this.panel, "panel");
+            this.panel.Name = "panel";
             // 
             // Form1
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.n_holes);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox n_holes;
+        private System.Windows.Forms.TableLayoutPanel panel;
     }
 }
--- memory allocation/Form1.cs
+++ memory allocation/Form1.cs
@@ -12,9 +12,33 @@ namespace memory_allocation
 {
     public partial class Form1 : Form
     {
+        int n_holes_int;
+        List<TextBox> hole_sizes = new List<TextBox>();
+        List<TextBox> hole_adds = new List<TextBox>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void n_holes_TextChanged(object sender, EventArgs e)
+        {
+            if (n_holes.Text == "") return;
+            n_holes_int = Int32.Parse(n_holes.Text);
+            panel.Controls.Clear();
+            panel.RowCount = n_holes_int + 1;
+            panel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 30F));
+            panel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 30F));
+            panel.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            panel.Controls.Add(new Label() { Text = "hole size" }, 0, 0);
+            panel.Controls.Add(new Label() { Text = "hole address" }, 1, 0);
+            for (int i = 1; i <= n_holes_int; ++i)
+            {
+                panel.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+                hole_sizes.Add(new TextBox());
+                hole_adds.Add(new TextBox());
+                panel.Controls.Add(hole_sizes[i-1], 0, i);
+                panel.Controls.Add(hole_adds[i - 1], 1, i);
+            }
         }
     }
 }
